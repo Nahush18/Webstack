349befcfef6c674ce5eaa6b7195836902605ba26
unknown <nahushthuse@gmail.com>
2024-03-02 10:37:47 +0530

    frontend

---
+++ src/Components/Home/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      {/* sticky-top = navbar remains at top even after scrolling */}
+      <nav className='header row sticky-top'>     
+      
+      </nav>
+    </div>
+  )
+}
+
+export default Header
